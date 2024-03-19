28267d0e9f77658ccd16414e8a85f8a46f93025d
AK <46664870+ak2110@users.noreply.github.com>
2024-03-19 19:42:10 -0400

    Update index.js

--- index.js
+++ index.js
@@ -5,16 +5,13 @@ const axios = require("axios");
 exports.handler = async (event) => {
   if (event.queryStringParameters) {
     const queryParams = event.queryStringParameters;
-    const verify_token = "QUINN";
-
-    // Parse params from the webhook verification request
+    const verify_token = process.env.VERIFY_TOKEN;
+    
     let mode = queryParams["hub.mode"];
     let token = queryParams["hub.verify_token"];
     let challenge = queryParams["hub.challenge"];
-
-    // Check if a token and mode were sent
+    
     if (mode && token) {
-      // Check the mode and token sent are correct
       if (mode ==="subscribe" && token === verify_token) {
         console.log("WEBHOOK_VERIFIED");
         return {
@@ -22,14 +19,13 @@ exports.handler = async (event) => {
           body: challenge,
         };
       } else {
-        // Responds with '403 Forbidden' if verify tokens do not match
         return {
           statusCode: 403,
         };
       }
     }
   } else {
-    const token = "EAAFpnO7WYJUBOz5aPMHDha32pXwZAcZB3RXeaAB6v2dc8wkSlHwZAKfRScARw2AGb7rCGWwgXEIQZBLRAurgTDcLRKFCRjEfPnHYzyKpUbYhgtuhllizeoYcJ0JEW4H6ZBfue37HypkZA38GF9EZATrs8MYBbjhITx2fZCfpn1p6gXzNaeAHcBnl785PApjTl5mh5fv4ufsbSxZCdQ8WXjv8ZD";
+    const token = process.env.WHATSAPP_TOKEN;
     const body = JSON.parse(event.body);
     if (body.object) {
       if (
@@ -41,8 +37,8 @@ exports.handler = async (event) => {
       ) {
         let phone_number_id =
           body.entry[0].changes[0].value.metadata.phone_number_id;
-        let from = body.entry[0].changes[0].value.messages[0].from; // extract the phone number from the webhook payload
-        let msg_body = body.entry[0].changes[0].value.messages[0].text.body; // extract the message text from the webhook payload
+        let from = body.entry[0].changes[0].value.messages[0].from;
+        let msg_body = body.entry[0].changes[0].value.messages[0].text.body;
         const tanslate = msg_body;
         await axios({
           method: "POST",
